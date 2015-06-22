11c3043f462932b721e1719a460efd25a5a95db4
machao <machao@home.(none)>
2015-06-22 17:27:00 +0800

    [mediaplayer:add,high]Async method.

--- src/com/machao/mediaplayer/MainActivity.java
+++ src/com/machao/mediaplayer/MainActivity.java
@@ -7,6 +7,7 @@ import java.io.IOException;
 import android.app.Activity;
 import android.media.AudioManager;
 import android.media.MediaPlayer;
+import android.media.MediaPlayer.OnPreparedListener;
 import android.os.Bundle;
 
 public class MainActivity extends Activity {
@@ -23,23 +24,32 @@ public class MainActivity extends Activity {
 		try {
 			mediaplayer.setDataSource("/mnt/sdcard/新娘不是我.mp3");
 			mediaplayer.setLooping(true);
-			mediaplayer.prepare();
-			mediaplayer.start();
+			mediaplayer.prepareAsync();
+			
+			mediaplayer.setOnPreparedListener(new OnPreparedListener() {
+				
+				@Override
+				public void onPrepared(MediaPlayer arg0) {
+					// TODO Auto-generated method stub
+					mediaplayer.start();
+				}
+			});
 			
 			System.out.println("播放OK！");
+			
 		} catch (Exception e) {
 			System.out.println("播放出错！");
 			e.printStackTrace();
 		}
 		
-/*		mediaplayer.setOnCompletionListener(new OnCompletionListener() {
-			
-			@Override
-			public void onCompletion(MediaPlayer mp) {
-				// TODO Auto-generated method stub
-				bt_play.setEnabled(true);
-			}
-		});*/
+		/*			mediaplayer.setOnCompletionListener(new OnCompletionListener() {
+		
+		@Override
+		public void onCompletion(MediaPlayer mp) {
+			// TODO Auto-generated method stub
+			mediaplayer.start();
+		}
+	});*/
 		
 		
 	}
