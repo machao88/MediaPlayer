c7fe1ecf071a596f103b76b5c3dea1cfce671df6
machao <machao@home.(none)>
2015-06-22 17:07:25 +0800

    [MediaPlayer:add,high]Add a simple music playing programme.

---
+++ src/com/machao/mediaplayer/MainActivity.java
@@ -0,0 +1,57 @@
+package com.machao.mediaplayer;
+
+
+
+import java.io.IOException;
+
+import android.app.Activity;
+import android.media.AudioManager;
+import android.media.MediaPlayer;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+	MediaPlayer mediaplayer;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		
+		mediaplayer = new MediaPlayer();
+		mediaplayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
+		try {
+			mediaplayer.setDataSource("/mnt/sdcard/新娘不是我.mp3");
+			mediaplayer.setLooping(true);
+			mediaplayer.prepare();
+			mediaplayer.start();
+			
+			System.out.println("播放OK！");
+		} catch (Exception e) {
+			System.out.println("播放出错！");
+			e.printStackTrace();
+		}
+		
+/*		mediaplayer.setOnCompletionListener(new OnCompletionListener() {
+			
+			@Override
+			public void onCompletion(MediaPlayer mp) {
+				// TODO Auto-generated method stub
+				bt_play.setEnabled(true);
+			}
+		});*/
+		
+		
+	}
+
+	@Override
+	protected void onDestroy() {
+		// TODO Auto-generated method stub
+		super.onDestroy();
+		mediaplayer.stop();
+		mediaplayer.release();
+	}
+
+
+
+}
