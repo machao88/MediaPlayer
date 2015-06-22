16d9806f2a79aff7b47a2b6197310a1e965bcbeb
machao <machao@home.(none)>
2015-06-22 21:40:18 +0800

    [video:add,high]surfaceview video is OK! test through Note3

--- src/com/machao/mediaplayer/MainActivity.java
+++ src/com/machao/mediaplayer/MainActivity.java
@@ -8,9 +8,13 @@ import android.app.Activity;
 import android.media.AudioManager;
 import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+import android.view.View;
 
 public class MainActivity extends Activity {
 	MediaPlayer mediaplayer;
+	private SurfaceView sv;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -18,30 +22,47 @@ public class MainActivity extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		
+		sv = (SurfaceView)findViewById(R.id.sv);
+		sv.getHolder().setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
+		
 		mediaplayer = new MediaPlayer();
+		
 		mediaplayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
+		
+		
+		mediaplayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+			
+			@Override
+			public void onCompletion(MediaPlayer mp) {
+				// TODO Auto-generated method stub
+				mediaplayer.start();
+			}
+		});
+		
+		
+	}
+	
+	public void play(View view){
+		
 		try {
-			mediaplayer.setDataSource("/mnt/sdcard/新娘不是我.mp3");
-			mediaplayer.setLooping(true);
-			mediaplayer.prepare();
-			mediaplayer.start();
+			mediaplayer.setDisplay(sv.getHolder());
+			mediaplayer.setDataSource("/storage/extSdCard/mp3/渡情.flv");
+//			mediaplayer.setLooping(true);
+			mediaplayer.prepareAsync();
+			mediaplayer.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+				
+				@Override
+				public void onPrepared(MediaPlayer mp) {
+					// TODO Auto-generated method stub
+					mp.start();
+				}
+			});
 			
 			System.out.println("播放OK！");
 		} catch (Exception e) {
 			System.out.println("播放出错！");
 			e.printStackTrace();
 		}
-		
-/*		mediaplayer.setOnCompletionListener(new OnCompletionListener() {
-			
-			@Override
-			public void onCompletion(MediaPlayer mp) {
-				// TODO Auto-generated method stub
-				bt_play.setEnabled(true);
-			}
-		});*/
-		
-		
 	}
 
 	@Override
