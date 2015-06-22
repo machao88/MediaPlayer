2e6a590485ac198ce7373e7672400da8e74719a5
machao <machao@home.(none)>
2015-06-23 00:12:14 +0800

    [position:add,high]add keep last position of video playing.

--- src/com/machao/mediaplayer/MainActivity.java
+++ src/com/machao/mediaplayer/MainActivity.java
@@ -9,12 +9,14 @@ import android.media.AudioManager;
 import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.SurfaceHolder;
+import android.view.SurfaceHolder.Callback;
 import android.view.SurfaceView;
 import android.view.View;
 
 public class MainActivity extends Activity {
 	MediaPlayer mediaplayer;
 	private SurfaceView sv;
+	private static int progress = 0;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -24,11 +26,37 @@ public class MainActivity extends Activity {
 		
 		sv = (SurfaceView)findViewById(R.id.sv);
 		sv.getHolder().setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
+		sv.getHolder().addCallback(new Callback() {
+			
+			@Override
+			public void surfaceDestroyed(SurfaceHolder arg0) {
+				// TODO Auto-generated method stub
+				if(mediaplayer != null && mediaplayer.isPlaying()){
+					progress = mediaplayer.getCurrentPosition();
+					mediaplayer.stop();
+				}
+			}
+			
+			@Override
+			public void surfaceCreated(SurfaceHolder arg0) {
+				// TODO Auto-generated method stub
+				if(progress > 0) {
+					//play(null);
+				}
+			}
+			
+			@Override
+			public void surfaceChanged(SurfaceHolder arg0, int arg1, int arg2, int arg3) {
+				// TODO Auto-generated method stub
+				
+			}
+		});
 		
 		mediaplayer = new MediaPlayer();
 		
 		mediaplayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
 		
+
 		
 		mediaplayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
 			
@@ -45,8 +73,8 @@ public class MainActivity extends Activity {
 	public void play(View view){
 		
 		try {
-			mediaplayer.setDisplay(sv.getHolder());
 			mediaplayer.setDataSource("/storage/extSdCard/mp3/渡情.flv");
+			mediaplayer.setDisplay(sv.getHolder());
 //			mediaplayer.setLooping(true);
 			mediaplayer.prepareAsync();
 			mediaplayer.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
@@ -55,6 +83,7 @@ public class MainActivity extends Activity {
 				public void onPrepared(MediaPlayer mp) {
 					// TODO Auto-generated method stub
 					mp.start();
+					mp.seekTo(progress);
 				}
 			});
 			
@@ -71,6 +100,7 @@ public class MainActivity extends Activity {
 		super.onDestroy();
 		mediaplayer.stop();
 		mediaplayer.release();
+		progress = 0;
 	}
 
 
